bf0d21880c39c309831d5dc46c8b3cfb61ccd0ee
Elise-IPSA <elise.rigouin@ipsa.fr>
2025-11-17 15:20:26 +0100

    Add files via upload

---
+++ principal.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'Projet'))
+
+from Projet.interface.fenetre import FenetrePrincipale
+from Projet.models.simu import Simulation
+from PySide6.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    simulation = Simulation()
+
+    fenetre = FenetrePrincipale(simulation)
+    fenetre.showMaximized()
+
+    sys.exit(app.exec())
